9ea252be1c19d82c70bd6a967dbc9abc1617e4cc
Trần Trung <nguyentrantrung1986@gmail.com>
2016-02-27 22:18:52 -0800

    click mouse and move plane1

--- Game1945/src/GameWindow.java
+++ Game1945/src/GameWindow.java
@@ -1,26 +1,26 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 /**
  * Created by TrungNT on 2/27/2016.
  */
-public class GameWindow extends Frame implements KeyListener, Runnable{
+public class GameWindow extends Frame implements MouseListener, KeyListener, Runnable{
 
     BufferedImage background;
-    BufferedImage plane4;
-    int xPlane, yPlane;
+    BufferedImage plane4, plane1;
+    int xPlane4, yPlane4, xPlane1, yPlane1;
     int direction=0;
 
     public GameWindow()
     {
-        xPlane = 200;
-        yPlane = 300;
+        xPlane4 = 200;
+        yPlane4 = 300;
+
+        xPlane1 = 250;
+        yPlane1 = 250;
         this.setTitle("This is Game 1945");
         this.setSize(400,640);
         this.setVisible(true);
@@ -35,6 +35,7 @@ public class GameWindow extends Frame implements KeyListener, Runnable{
         try {
             background = ImageIO.read(new File("Resouces/Background.png"));
             plane4 = ImageIO.read(new File("Resouces/PLANE4.png"));
+            plane1 = ImageIO.read(new File("Resouces/PLANE1.png"));
 
         }catch (Exception e)
         {
@@ -42,7 +43,7 @@ public class GameWindow extends Frame implements KeyListener, Runnable{
         }
 
         this.addKeyListener(this);
-
+        this.addMouseListener(this);
 
         repaint();
 
@@ -53,8 +54,8 @@ public class GameWindow extends Frame implements KeyListener, Runnable{
     {
         g.drawImage(background, 0, 0, null);
         g.drawLine(0, 0, 100, 100);
-        g.drawImage(plane4, xPlane, yPlane, null);
-
+        //g.drawImage(plane4, xPlane4, yPlane4, null);
+        g.drawImage(plane1, xPlane1, yPlane1, null);
 
     }
 
@@ -65,18 +66,18 @@ public class GameWindow extends Frame implements KeyListener, Runnable{
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyChar()=='w')
+        if(e.getKeyChar()=='w') //di chuyen len tren
         {
             //yPlane -= 30;
             //repaint();
             direction = 1;
         }
 
-        if(e.getKeyChar() == 's')
+        if(e.getKeyChar() == 's') //di chuyen xuong duoi
             direction = 2;
-        if(e.getKeyChar() == 'd')
+        if(e.getKeyChar() == 'd') //di chuyen sang phai
             direction = 3;
-        if(e.getKeyChar() == 'a')
+        if(e.getKeyChar() == 'a') //di chuyen sang trai
             direction = 4;
     }
 
@@ -96,17 +97,19 @@ public class GameWindow extends Frame implements KeyListener, Runnable{
             }
             else
             if(direction==1){
-                yPlane -=3;
+                yPlane4 -=3;
             }
             else
             if(direction==2)
-                yPlane +=3;
+                yPlane4 +=3;
             else
             if(direction==3)
-                xPlane+=3;
+                xPlane4+=3;
             else
             if (direction==4)
-                xPlane-=3;
+                xPlane4-=3;
+
+
             repaint();
 
             try {
@@ -115,5 +118,32 @@ public class GameWindow extends Frame implements KeyListener, Runnable{
                 e.printStackTrace();
             }
         }
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        xPlane1 = e.getX();
+        yPlane1 = e.getY();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
