f6315686289729c484094cd8fab3cbdcfe4c3262
Trần Trung <nguyentrantrung1986@gmail.com>
2016-03-07 17:28:45 -0800

    define move diagonal in BulletDiagonal class

--- src/BulletDiagonal.java
+++ src/BulletDiagonal.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 /**
  * Created by TrungNT on 3/7/2016.
  */
@@ -8,9 +10,24 @@ public class BulletDiagonal extends Bullet implements IMove{
         super(x, y, speed, planeType);
     }
 
+    //dinh nghia phuong thuc di chuyen cheo trai phai
     @Override
     public void move() {
+        if(this.speed > 0) { //di chuyen cheo trai
+            this.postionY += this.speed;
+            this.postionX -= this.speed;
+        }
+        else{ //di chuyen cheo phai
+            this.postionY += this.speed;
+            this.postionX += this.speed;
+        }
+    }
 
+    //dinh nghia phuong thuc ve hinh draw cua class BulletDiagonal
+    public void draw(Graphics g)
+    {
+        //do ham draw da dinh nghia o class cha Bullet nen va class nay ke thua nen su dung lai duoc
+        super.draw(g);
     }
 
 }
